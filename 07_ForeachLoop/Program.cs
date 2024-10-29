874d4004ff546cd80c84b27970cd4cef8aca9f5e
CdevWizards <ubahmet32@hotmail.com>
2024-10-29 22:13:43 +0300

    Foreach

--- 07_ForeachLoop/Program.cs
+++ 07_ForeachLoop/Program.cs
@@ -45,7 +45,7 @@ namespace Arrays
 
         // List<int> numbers = new List<int>()
         // {
-        //     1, 2, 3, 4, 5, 6, 7
+        //     2, 3, 4, 5, 6, 7, 8
         // };
         // foreach(int sayi in numbers)
         // {
