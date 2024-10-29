b95d552e0c557a8a7088db8a24fe04a454c24c53
CdevWizards <ubahmet32@hotmail.com>
2024-10-29 13:14:59 +0300

    Loops

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,149 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region For Döngüsü
+
+            //int i;
+
+            //for (i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("C# Kampı");
+            //}
+
+            //for (int i = 1; i <= 25; i++)
+            //{ 
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 2; i <= 55; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine("Lütfen ekrana yazılmasını istediğiniz adeti giriniz: ");
+            //int finValue = int.Parse(Console.ReadLine());
+
+            //for(int i = 0; i < finValue; i++)
+            //{
+            //    Console.WriteLine("Cumhuriyet, yeni ve sağlam esaslariyle, Türk milletini emin ve sağlam bir istikbal yoluna koyduğu kadar, asıl fikirlerde ve ruhlarda yarattığı güvenlik itibariyle, büsbütün yeni bir hayatın müjdecisi olmuştur");
+            //}
+
+            #endregion
+
+            #region -- For --
+
+            //for (int i = 0; i <= 75; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int totalvalue = 0;
+            //for (int i = 1; i <= 15; i++)
+            //{
+            //    totalvalue += i;
+            //}
+
+            //Console.WriteLine(totalvalue);
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i < 10; i++) 
+            //{
+            //    if (i % 2 == 0) 
+            //    { 
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+            //    }   
+            //}
+
+            //Console.WriteLine("-------------");
+            //Console.WriteLine(totalValue);
+
+            //int count = 0;
+            //for (int i = 1; i <= 60; i++)
+            //{
+            //    if (i % 9 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine(count);
+
+            //int bacterium = 1;
+            //for (int i = 1; i <= 48; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + ",Saat Sonunda: " + );
+            //}
+
+
+            #endregion
+
+            #region While Döngüsü
+
+            //int i = 1;
+
+            //while (i <= 12)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler");
+            //    i++;
+            //}
+
+            //int i = 1;
+            //while (i <= 18)
+            //{
+            //    if (i % 6 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //        i++;
+            //    }
+            //}
+
+            //int i = 1, sum = 0;
+            //while (i <= 10) 
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+            #endregion
+
+            #region Örnek Soru
+
+            //int number, total = 0, i, figure;
+
+            //Console.Write("Lütfen 3 basamaklı sayı giriniz: ");
+            //number = int.Parse(Console.ReadLine());
+
+            //for (i = 0; i < 8; i++)
+            //{
+            //    figure = number % 20;
+            //    number = number - figure;
+            //    number = number / 20;
+            //    total += figure;
+            //}
+
+            //Console.WriteLine("Toplam: " + total);
+
+            #endregion
+        }
+    }
+}
