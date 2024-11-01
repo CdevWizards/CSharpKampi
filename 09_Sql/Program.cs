d06ab4828ec1a496bbf477cb26b29515028030f0
CdevWizards <ubahmet32@hotmail.com>
2024-11-01 11:35:05 +0300

    09_Sql

---
+++ 09_Sql/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace DatabaseProject 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ADO.NETs
+
+            Console.WriteLine("---C# Ürün Kategori Bilgi Sistemi---");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            string? tableNumber;
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.WriteLine("Lütfen Getirmek istediğiniz tablo numarasını giriniz:");
+            tableNumber = Console.ReadLine();
+ Console.WriteLine("-------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=ULUSOY\\SQLEXPRESS;initial Catalog=EgitimKampiDb;integrated security=true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * From TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);   // C# ile oluşturduğum sorgu ile sql veritabanı arasında köprü görevi görür.
+            DataTable dataTable = new DataTable();  // Verilere ram belleğe almamızı sağlayacak...
+            adapter.Fill(dataTable);
+            connection.Close();
+            foreach(DataRow row in dataTable.Rows)
+            {
+                    foreach(var item in row.ItemArray)
+                    {
+                        Console.WriteLine(item.ToString());
+                    }
+            }
+            Console.Read();
+        }
+    }
+}
