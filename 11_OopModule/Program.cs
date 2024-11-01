a3f8ad727f21407bd83a31d34169e589ec2b6459
CdevWizards <ubahmet32@hotmail.com>
2024-11-01 17:10:50 +0300

    First Lesson

---
+++ 11_OopModule/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace DatabaseCrud 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+        }
+
+    }
+}
